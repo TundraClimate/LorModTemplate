cac96c279701534bdaf5d0694a8f1aa10a288c91
TundraClimate <tundra.tundere@gmail.com>
2026-02-07 13:30:34 +0900

    ✨feat: Improve init.bat

--- init.bat
+++ init.bat
@@ -1,32 +1,109 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 :: テンプレ
 set "BASEDIR=%~dp0"
 call "%BASEDIR%\env.bat"
 call "%BASEDIR%\usr.bat"
 
-:: ローカルのMods/にリンクを貼る
-if not exist "%LOR_MOD_DIR%\%ID%\" (
-    mklink /D "%LOR_MOD_DIR%\%ID%\" "%BASEDIR%\Invitation\"
+echo Check %%LOR_DIR%% validity
 
-    echo Invitation link successfully created
+:: %%LOR_DIR%%が正規かの確認
+if not exist "%LOR_DIR%" (
+    echo Error: Found the invalid LOR_DIR
+    echo To fix: Replace the LOR_DIR= path that invalid 'LibraryOfRuina_Data' foldor into the accurate path
+    echo Find accurate path: Steam application ^> 'Library of Ruina' ^> Configure ^> Management ^> Show Local files
+    echo.
+    echo ...and Try again here
+
+    pause
+    exit /b
 ) else (
-    echo Invitation link already created
+    echo Find the valid LOR_DIR: Passed
+
+    if not exist "%LOR_DATA_DIR%" (
+        echo Error: Found the invalid LOR_DATA_DIR
+        echo To fix: Replace the LOR_DATA_DIR= path that invalid 'LibraryOfRuina_Data/Managed' folder into the accurate path
+        echo Find accurate path: Steam application ^> 'Library of Ruina' ^> Configure ^> Management ^> Show Local files
+        echo.
+        echo ...and Try again here
+
+        pause
+        exit /b
+    )
+
+    echo Find the valid LOR_DATA_DIR: Passed
+
+    if not exist "%LOR_MOD_DIR%" (
+        echo Error: Found the invalid LOR_DATA_DIR
+        echo To fix: Replace the LOR_MOD_DIR= path that invalid 'LibraryOfRuina_Data/Mods' folder into the accurate path
+        echo Find accurate path: Steam application ^> 'Library of Ruina' ^> Configure ^> Management ^> Show Local files
+        echo.
+        echo ...and Try again here
+
+        pause
+        exit /b
+    )
+
+    echo Find the valid LOR_MOD_DIR: Passed
 )
 
+echo.
+echo Create symlink to \Mods\
+
+:: ローカルのMods/にリンクを貼る
+for %%A in ("%LOR_DIR%") do (
+    :: ドライブ名にアクセス出来るか
+    if not "%%~dA" == "" if not "%%~pA" == "%%~dpA" (
+        :: ドライブ名を指定してフォーマット形式を取得
+        for /f "skip=1" %%F in ('wmic logicaldisk where "DeviceID='%%~dA'" get FileSystem') do (
+            if "%%F" == "NTFS" (
+                set FS=%%F
+            )
+        )
+
+        if "!FS!" == "NTFS" (
+            if not exist "%LOR_MOD_DIR%\%ID%\" (
+                mklink /D "%LOR_MOD_DIR%\%ID%\" "%BASEDIR%\Invitation\"
+
+                echo Invitation link successfully created for \Mods\
+            ) else (
+                echo Invitation link already created for \Mods\
+            )
+        ) else (
+            echo Skip symlink: Drive %%~dA is not supported the symlink
+        )
+    ) else (
+        echo Skip symlink: LOR_DIR is not absolute path, the drive access denied
+        echo To fix: Put the absolute path to the LOR_DIR
+    )
+)
+
+echo.
+echo Start copy the local assemblies
+
+if not exist "%BASEDIR%\src\libs\" (
+    echo A \libs\ not found
+    mkdir "%BASEDIR%\src\libs\"
+
+    if exist "%BASEDIR%\src\libs\" (
+        echo A \libs\ successfully created
+    ) else (
+        echo Error: Failed create a \libs\
+
+        pause
+        exit /b
+    )
+)
 
 :: ライブラリ参照のコピペ
-if not exist "%BASEDIR%\src\libs\" (
-    mkdir "%BASEDIR%\src\libs\"
-    copy /Y "%LOR_DATA_DIR%\System.dll" "%BASEDIR%\src\libs\System.dll"
-    copy /Y "%LOR_DATA_DIR%\System.Core.dll" "%BASEDIR%\src\libs\System.Core.dll"
-    copy /Y "%LOR_DATA_DIR%\mscorlib.dll" "%BASEDIR%\src\libs\mscorlib.dll"
-    copy /Y "%LOR_DATA_DIR%\Assembly-CSharp.dll" "%BASEDIR%\src\libs\Assembly-CSharp.dll"
+call :copyasm "%LOR_DATA_DIR%\System.dll" "%BASEDIR%\src\libs\System.dll"
+call :copyasm "%LOR_DATA_DIR%\System.Core.dll" "%BASEDIR%\src\libs\System.Core.dll"
+call :copyasm "%LOR_DATA_DIR%\mscorlib.dll" "%BASEDIR%\src\libs\mscorlib.dll"
+call :copyasm "%LOR_DATA_DIR%\Assembly-CSharp.dll" "%BASEDIR%\src\libs\Assembly-CSharp.dll"
 
-    echo Local assemblies successfully copied
-) else (
-    echo Local assemblies already copied
-)
+echo.
+echo Replace the all template syntax
 
 :: 既存テンプレートの文字置き換え
 if exist "%BASEDIR%\src\.csproj" (
@@ -41,9 +118,20 @@ if exist "%BASEDIR%\src\.csproj" (
 
     echo All template syntax successfully replaced
 ) else (
-    echo All template syntax already replaced
+    if exist "%BASEDIR%\src\%ID%.csproj" (
+        echo All template syntax already replaced
+    ) else (
+        echo Error: The template file not found
+        echo Error: This template has broken file
+        echo.
+        echo Try again at first step
+
+        pause
+        exit /b
+    )
 )
 
+echo.
 echo --ALL TASK COMPLETE--
 pause
 
@@ -51,4 +139,13 @@ exit /b
 
 :replace
 powershell -Command "(Get-Content '%~1') -replace '%~2','%~3' | Set-Content '%~1'"
+exit /b
+
+:copyasm
+if not exist "%~2" (
+    copy /Y "%~1" "%~2"
+    echo A file copied to %~p2: %~nx1
+) else (
+    echo A file was already copied: %~nx1
+)
 exit /b
