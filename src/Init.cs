58f890fb5b807ab3141ca381d8eb336feb9bdc9d
TundraClimate <tundra.tundere@gmail.com>
2026-02-24 05:18:43 +0900

    ✨feat: Add MutePatch

--- src/Init.cs
+++ src/Init.cs
@@ -13,14 +13,46 @@ public class __MODNAME__ : ModInitializer
 
     public override void OnInitializeMod()
     {
+        /*
+        ApplyHarmonyPatch();
+        */
     }
 
-    /* public static void ApplyHarmonyPatch()
+    /* 
+    public static void ApplyHarmonyPatch()
     {
         Harmony harmony = new Harmony(__MODNAME__.packageId);
         foreach (Type type in typeof(PatchClass).GetNestedTypes(AccessTools.all))
         {
             harmony.CreateClassProcessor(type).Patch();
         }
-    } */
+    }
+
+    private static void MutePatch()
+    {
+        Harmony harmony = new Harmony(__MODNAME__.packageId + ".MutePatch");
+
+        MethodInfo postfix = typeof(__MODNAME__).GetMethod("MuteSameAssembly", BindingFlags.Static | BindingFlags.NonPublic);
+
+        harmony.Patch(typeof(Mod.ModContentManager).GetMethod("GetErrorLogs"), postfix: new HarmonyMethod(postfix));
+    }
+
+    private static void MuteSameAssembly(ref List<string> __result)
+    {
+        List<string> bin = new List<string>();
+
+        foreach (string err in __result)
+        {
+            if (err.Contains("The same assembly name already exists."))
+            {
+                bin.Add(err);
+            }
+        }
+
+        foreach (string trash in bin)
+        {
+            __result.Remove(trash);
+        }
+    }
+    */
 }
