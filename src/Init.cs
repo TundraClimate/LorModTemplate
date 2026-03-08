fbe8f00935a3eb890af03983181b09d9bcc730d9
TundraClimate <tundra.tundere@gmail.com>
2026-03-08 13:12:49 +0900

    ✨feat: Includes 0Harmony.dll

--- src/Init.cs
+++ src/Init.cs
@@ -1,24 +1,15 @@
-/* using System;
-using HarmonyLib; */
+using System;
+using HarmonyLib;
 
 public class __MODNAME__ : ModInitializer
 {
-    public static string packageId
-    {
-        get
-        {
-            return "__MODNAME__";
-        }
-    }
+    public static string packageId => "__MODNAME__";
 
     public override void OnInitializeMod()
     {
-        /*
         ApplyHarmonyPatch();
-        */
     }
 
-    /* 
     public static void ApplyHarmonyPatch()
     {
         Harmony harmony = new Harmony(__MODNAME__.packageId);
@@ -27,32 +18,4 @@ public class __MODNAME__ : ModInitializer
             harmony.CreateClassProcessor(type).Patch();
         }
     }
-
-    private static void MutePatch()
-    {
-        Harmony harmony = new Harmony(__MODNAME__.packageId + ".MutePatch");
-
-        MethodInfo postfix = typeof(__MODNAME__).GetMethod("MuteSameAssembly", BindingFlags.Static | BindingFlags.NonPublic);
-
-        harmony.Patch(typeof(Mod.ModContentManager).GetMethod("GetErrorLogs"), postfix: new HarmonyMethod(postfix));
-    }
-
-    private static void MuteSameAssembly(ref List<string> __result)
-    {
-        List<string> bin = new List<string>();
-
-        foreach (string err in __result)
-        {
-            if (err.Contains("The same assembly name already exists."))
-            {
-                bin.Add(err);
-            }
-        }
-
-        foreach (string trash in bin)
-        {
-            __result.Remove(trash);
-        }
-    }
-    */
 }
--- src/PatchClass.cs
+++ src/PatchClass.cs
@@ -1,3 +1,27 @@
+using System.Collections.Generic;
+using HarmonyLib;
+
 public static class PatchClass
 {
+    [HarmonyPatch(typeof(Mod.ModContentManager), "GetErrorLogs")]
+    static class MutePatch
+    {
+        static void Postfix(ref List<string> __result)
+        {
+            List<string> bin = new List<string>();
+
+            foreach (string err in __result)
+            {
+                if (err.Contains("The same assembly name already exists."))
+                {
+                    bin.Add(err);
+                }
+            }
+
+            foreach (string trash in bin)
+            {
+                __result.Remove(trash);
+            }
+        }
+    }
 }
